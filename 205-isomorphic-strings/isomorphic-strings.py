3066813a1ab7ac4ba48d2b77726e7aefb7033979
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-06-26 15:27:03 +0530

    Time: 3 ms (95.29%) | Memory: 18 MB (37.33%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,26 +1,23 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         hashNums = {}
-
-        for i in range(len(s)):
-            ch = s[i]
-            if ch in hashNums:
-                if hashNums[ch] != t[i]:
-                    return False
-            else:
-                hashNums[ch] = t[i]
-
         hashNums2 = {}
 
-        for i in range(len(t)):
-            ch = t[i]
-            if ch in hashNums2:
-                if hashNums2[ch] != s[i]:
+        for i in range(len(s)):
+            ch1 = s[i]
+            ch2 = t[i]
+            if ch1 in hashNums:
+                if hashNums[ch1] != ch2:
+                    return False
+            elif ch2 in hashNums2:
+                if hashNums2[ch2] != ch1:
                     return False
             else:
-                hashNums2[ch] = s[i]
-
+                hashNums[ch1] = ch2
+                hashNums2[ch2] = ch1
         return True
+
+     
 
                         
                 
