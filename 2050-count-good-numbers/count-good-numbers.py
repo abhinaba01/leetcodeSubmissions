e9b94dfe0fd655ac3f35e5ec47b7493e9ca388ce
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-06-27 17:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (5.75%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def myPow(self, x: float, n: int) -> float:
+
+        MOD=1000000007
+
+        if n == 0:
+            return 1
+        
+        else:
+            res = self.myPow(x,n//2)
+            if n & 1 == 0:
+                
+                return (res * res) % MOD
+            else:
+                 return (res * x * res) % MOD
+       
+    def countGoodNumbers(self, n: int) -> int:
+
+        MOD=1000000007
+
+        
+        nOdd = n // 2
+        nEven = 0
+        res = 0
+        if n%2 == 0:
+            nEven = nOdd
+        else:
+            nEven = nOdd + 1
+
+        res = self.myPow(5,nEven) * self.myPow(4,nOdd)
+        return res % (MOD)
+        
