591f2ee4e91b82ffb8e4f0fe14082b336e307230
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-01 16:56:13 +0530

    Time: 5 ms (5.04%) | Memory: 17.8 MB (37.83%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+
+        ans = []
+        path = []
+
+        def dfs(num,target,i):
+
+            if num == 0 and target == 0:
+                ans.append(path[:])
+                return
+                
+            elif num == 0 and target > 0:
+               
+                return
+               
+
+            else:
+                for j in range(i,10):
+                    path.append(j)
+                    print(path)
+                    dfs(num-1,target-j,j+1)
+                    path.pop()
+
+        dfs(k,n,1)
+        return ans
+
+
+
+
+           
+        
+
+
+        
