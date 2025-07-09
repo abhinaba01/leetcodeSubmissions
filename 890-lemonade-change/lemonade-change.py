6328129225878ddc5acb9c091f4103c45dee847c
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-09 21:14:41 +0530

    Time: 4 ms (50.10%) | Memory: 21.9 MB (77.66%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -4,32 +4,32 @@ class Solution:
         r = 0
         n = len(bills)
       
-        a,b,c = 0,0,0
+        count = {5:0,10:0}
       
         while r < n:
             bill = bills[r]
 
             if bill == 20:
                 
-                if a >= 1 and b >= 1:
-                    a -= 1
-                    b -= 1
+                if count[5] >= 1 and count[10] >= 1:
+                    count[5] -= 1
+                    count[10] -= 1
                 
-                elif a >= 3:
-                    a -= 3
+                elif count[5] >= 3:
+                    count[5] -= 3
                 else:
                     return False
-                c += 1
+                
 
             if bill == 10:
-                if a >= 1:
-                    a -= 1
+                if count[5] >= 1:
+                    count[5] -= 1
                 else:
                     return False
-                b += 1
+                count[10] += 1
 
             if bill == 5:
-                a += 1             
+                count[5] += 1             
             
             r += 1
         return True
