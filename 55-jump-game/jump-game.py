01396d4aa8ffb1219aa76e53f3a9dc6fb22e5fcf
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-11 13:49:19 +0530

    Time: 34 ms (48.51%) | Memory: 18.4 MB (82.24%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,32 +1,43 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        arr = [1] * n
+    
+
+        max_farthest = 0
+        for i in range(n):
+
+            if i > max_farthest:
+                return False
+            max_farthest = max(i+nums[i],max_farthest)
+
+        return True
+            
+
         
 
-        def dfs(i):
+        # def dfs(i):
 
-            if arr[i] == 0:
-                return False
+        #     if arr[i] == 0:
+        #         return False
 
-            if i == n-1:
-                return True
+        #     if i >= n-1:
+        #         return True
                
-            if i < (n-1) and nums[i] == 0:
-                arr[i] = 0
-                return False
+        #     if i < (n-1) and nums[i] == 0:
+        #         arr[i] = 0
+        #         return False
 
-            for j in range(1,nums[i] + 1):
+        #     for j in range(1,nums[i] + 1):
                 
-                if dfs(i+j):
-                    return True
+        #         if dfs(i+j):
+        #             return True
 
-            arr[i] = 0
+        #     arr[i] = 0
 
             
-            return False
+        #     return False
 
-        return dfs(0)
+        # return dfs(0)
 
 
 
