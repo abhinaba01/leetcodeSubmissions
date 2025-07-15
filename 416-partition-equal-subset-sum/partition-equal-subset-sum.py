3246a1596583061506cf0ba2b7ae8807ad181532
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-15 13:56:28 +0530

    Time: 912 ms (43.07%) | Memory: 156.4 MB (14.32%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+
+        sumArr = sum(nums)
+        if sumArr & 1 != 0:
+            return False
+
+        target = sumArr // 2
+        n = len(nums)
+
+        dp = {}
+
+        def dfs(i,currentSum):
+
+            if currentSum == 0:
+                dp[(i,currentSum)] = True
+                return True
+
+            if (i,currentSum) in dp:
+                return dp[(i,currentSum)]
+
+            if i == n or currentSum < 0:
+                return False
+            
+           
+
+            dp[(i,currentSum)] = dfs(i+1,currentSum-nums[i]) or dfs(i+1,currentSum)
+            return dp[(i,currentSum)]
+
+        return dfs(0,target)
+
+                
+
+
+            
+        
