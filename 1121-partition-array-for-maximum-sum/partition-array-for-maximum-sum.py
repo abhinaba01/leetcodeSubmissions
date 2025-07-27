7a6868f1d060573283cef39b5844035d06429af0
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-27 18:12:16 +0530

    Time: 327 ms (43.74%) | Memory: 18.9 MB (31.44%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -3,14 +3,14 @@ class Solution:
 
         n = len(arr)
 
-        dp = [0 for _ in range(n+1)]
+        dp = [-1 for _ in range(n+1)]
 
         def dfs(i):
          
             if i == n:
                 return 0
 
-            if dp[i] != 0:
+            if dp[i] != -1:
                 return dp[i]
             maxCost = 0
             maxVal = 0
