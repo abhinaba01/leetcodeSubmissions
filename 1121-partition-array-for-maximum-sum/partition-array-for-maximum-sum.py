9059943758b46f6b5d8630d39a5aa1e22f3ad8b9
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-07-27 18:11:47 +0530

    Time: 332 ms (42.34%) | Memory: 18.9 MB (28.42%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -13,9 +13,11 @@ class Solution:
             if dp[i] != 0:
                 return dp[i]
             maxCost = 0
+            maxVal = 0
             for j in range(i,min(i+k,n)):
               
-                cost = (max(arr[i:j+1]) * (j+1-i)) + dfs(j+1)
+                maxVal  = max(maxVal,arr[j])
+                cost = maxVal * (j+1-i) + dfs(j+1)
                 maxCost = max(maxCost,cost)
             
             dp[i] = maxCost
