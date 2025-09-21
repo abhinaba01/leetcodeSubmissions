9c27215d3ca7f4fd3817c89f5baf44edea1ce40d
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-09-21 13:33:08 +0530

    Time: 3 ms (75.86%) | Memory: 19.5 MB (35.04%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -2,45 +2,40 @@ from typing import List
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = [[] for _ in range(numCourses)]
-        for course, pre in prerequisites:
-            graph[pre].append(course)
 
-        vis = [0] * numCourses  # 0: unvisited, 1: visiting, 2: visited
-        order = []
-        has_cycle = False
+        adj = [[] for _ in range(numCourses)]
+
+        for start, end in prerequisites:
+            adj[start].append(end)
+
+        topSort = []
+        path = set()
+        visit = set()
 
         def dfs(node):
-            nonlocal has_cycle
-            if vis[node] == 1:
-                # cycle detected
-                has_cycle = True
-                return
-            if vis[node] == 2:
-                return
 
-            vis[node] = 1  # mark as visiting
-
-            for neighbor in graph[node]:
-                dfs(neighbor)
-                if has_cycle:
-                    return
-
-            vis[node] = 2  # mark as visited
-            order.append(node)  # post-order appending
-
+            if node in path:
+                return False
+            if node in visit:
+                return True
+            path.add(node)
+            
+            for neighbours in adj[node]:
+                if not dfs(neighbours):
+                    return False
+            topSort.append(node)
+            visit.add(node)
+            path.remove(node)
+            return True
+        
         for i in range(numCourses):
-            if vis[i] == 0:
-                dfs(i)
-                if has_cycle:
-                    return []
-
-        return order[::-1]  # reverse post-order
-
-
-
+            if not dfs(i):
+                return []
+        
+        return topSort
 
         
+
 
 
 
