5508680c452aa4827c3795756aa3b0d95b1047d5
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-09-25 19:44:53 +0530

    Time: 2162 ms (5.04%) | Memory: 21.4 MB (7.92%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+
+        visited = set()
+        neighbors = [(0,1),(0,-1),(-1,0),(1,0)]
+        rows = len(heights)
+        cols = len(heights[0])
+
+        def bfs(r,c,k,visited):
+            if r == rows -1 and c == cols - 1:
+                return True
+            if (r,c) in visited:
+                return False
+            visited.add((r,c)) 
+            neighbors = [(0,1),(0,-1),(-1,0),(1,0)]
+            for dr,dc in neighbors:
+                if min(r+dr,c+dc) < 0 or r + dr == rows or c + dc == cols:
+                    continue
+                if (r+dr,c+dc) not in visited:
+                    if abs(heights[r+dr][c+dc] - heights[r][c]) <= k:
+                        if bfs(r+dr,c+dc,k,visited):
+                            return True
+                        
+            return False
+
+        
+        low = 0
+        high = 10 ** 6
+
+        while low < high:
+            mid = (low + high) // 2
+            visited = set()
+            if bfs(0,0,mid,visited):
+                high = mid
+            else:
+                low = mid + 1
+        return low
+
+
+        
