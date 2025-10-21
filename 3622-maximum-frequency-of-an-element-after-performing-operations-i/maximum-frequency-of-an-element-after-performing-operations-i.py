1533e68a24358d6f3151e01218c6c5d6f7dc16c0
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-10-21 15:35:13 +0530

    Time: 1787 ms (6.98%) | Memory: 33.5 MB (62.21%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -0,0 +1,52 @@
+from collections import Counter
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+
+        n = len(nums)
+
+        cnt = Counter(nums)
+        res = float("-inf")
+
+
+        def findLower(num):
+            low,high = 0,n-1
+
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] >= num:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return low
+
+        def findUpper(num):
+            low,high = 0,n-1
+
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] <= num:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return high
+
+        minNum = min(nums)
+        maxNum = max(nums)
+
+        nums.sort()
+
+        
+
+        for i in range(minNum,maxNum + 1):
+            l = findLower(i-k)
+            r = findUpper(i+k)
+            ans = min(r - l + 1, numOperations+cnt[i])
+            res = max(res,ans)
+        
+        return res
+
+
+            
+
+
+        
