bc6cefea74d1c943fdbfbaef8ac9724e680148fc
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-19 11:09:47 +0530

    Time: 1523 ms (6.39%) | Memory: 17.5 MB (99.59%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -2,25 +2,12 @@ class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
 
         n = len(arr)
+        dp = [0] * (n + 1)
 
-        dp = [-1 for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,min(i,k)+1):
+               
+                dp[i] = max(dp[i-j] + max(arr[i-j:i])*j,dp[i])
+        return dp[n]
 
-        def dfs(i):
-         
-            if i == n:
-                return 0
-
-            if dp[i] != -1:
-                return dp[i]
-            maxCost = 0
-            maxVal = 0
-            for j in range(i,min(i+k,n)):
-              
-                maxVal  = max(maxVal,arr[j])
-                cost = maxVal * (j+1-i) + dfs(j+1)
-                maxCost = max(maxCost,cost)
-            
-            dp[i] = maxCost
-            return maxCost
-
-        return dfs(0)
+        
