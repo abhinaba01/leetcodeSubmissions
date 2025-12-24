30465271a9dfc1c5beb5d7d0990f4d8617aee460
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-24 09:41:27 +0530

    Time: 507 ms (21.05%) | Memory: 37.3 MB (23.44%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,30 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
 
-        n = len(prices)
-        dp = [[-1 for _ in range(n)] for _ in range(2)]
-      
-
-
-        def dfs(ind,buy):
-         
-            if ind == n:
-                return 0
-
-            if dp[buy][ind] != -1:
-                return dp[buy][ind]
-
-            if buy == 1:
-                profit= max(prices[ind] - fee + dfs(ind+1,0) , dfs(ind + 1 , 1))
-               
-            else:
-                profit =  max(-prices[ind] + dfs(ind+1,1), dfs(ind+1,0))
-
-            dp[buy][ind] = profit
-            return dp[buy][ind]
-               
-        return dfs(0,0)
- 
         
+        n = len(prices)
+        
+        dp = [[[float("-inf")]  for _ in range(2)] for _ in range(n + 1)]
 
+        for i in range(n):
+            dp[i][0] = 0
+        dp[0][1] = - prices[0]
+
+        for i in range(1,n):
+
+           
+             
+                dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+                dp[i][0] = max(dp[i-1][0] , dp[i-1][1] + prices[i] - fee )
+               
+        
+        return max(dp[n-1])
+
+        
         
