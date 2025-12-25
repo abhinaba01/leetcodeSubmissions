257f068d84f281e6b42612b6cd0b3f66636161a9
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-25 11:31:18 +0530

    Time: 99 ms (75.49%) | Memory: 19.2 MB (70.59%) - LeetSync

---
+++ 1555-number-of-ways-of-cutting-a-pizza/number-of-ways-of-cutting-a-pizza.py
@@ -0,0 +1,85 @@
+class Solution:
+    def ways(self, pizza: List[str], k: int) -> int:
+
+        r = len(pizza)
+        c = len(pizza[0])
+
+        freq_row = defaultdict(list)
+        freq_col = defaultdict(list)
+
+        MOD = 10 ** 9 + 7
+
+
+        for i in range(r):
+            for j in range(c):
+                if pizza[i][j] == 'A':
+                    freq_row[i].append(j)
+                    freq_col[j].append(i)
+        
+        def find(arr,n):
+            for num in arr:
+                if num >= n:
+                    return num
+            return -1
+        
+        @cache
+        def count(sr,sc,k):
+            start_r = -1
+            start_c = -1
+
+            ans = 0
+            a = 0
+
+            if k == 0:
+                for i in range(sr,r):
+                    if find(freq_row[i],sc) != -1:
+                        a = 1
+                        break
+
+
+                if a == 1:
+                    return 1
+                else:
+                    return 0              
+            
+       
+            
+            if sr == r or sc == c:
+                return 0
+
+            start_r , start_c = -1,-1
+            
+            for i in range(sr,r):
+                if find(freq_row[i],sc) != -1:
+                    start_r = i
+                    break
+                
+          
+            for j in range(sc,c):
+                if find(freq_col[j],sr) != -1:
+                    start_c = j
+                    break
+               
+
+            
+            if start_r == -1 or start_c == -1:
+                return 0
+            
+            
+         
+            for i in range(start_r,r):
+                ans = (ans + count(i+1,sc,k-1)) % MOD
+
+            for j in range(start_c,c):
+                ans = (ans + count(sr,j+1,k-1)) % MOD
+          
+            
+            return ans
+        
+        return count(0,0,k-1)
+
+
+
+          
+
+        
