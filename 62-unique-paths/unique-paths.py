ae33d9e00a77647b30b060ce89f878c0b0b87979
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-25 18:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.14%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,18 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        def factorial(k):
-            if (k == 1) or (k == 0):
-                return 1
-            else:
-                return k * factorial(k-1)
-
-        a = factorial(m+n - 2) // (factorial(m - 1) * factorial(n - 1))
-
-        return a
-
-
-
-
-
+        return (math.comb(m+n-2,m-1))
         
