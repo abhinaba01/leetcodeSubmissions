96781f8a4967f2286553fe3bf1bd28029af7d4ba
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-27 08:12:01 +0530

    Time: 183 ms (38.71%) | Memory: 43.6 MB (35.48%) - LeetSync

---
+++ 3464-maximize-total-cost-of-alternating-subarrays/maximize-total-cost-of-alternating-subarrays.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumTotalCost(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        dp = [[-1] * (2) for _ in range(n+1)]
+        
+        dp[0][0] = nums[0]
+        dp[0][1] = nums[0]
+
+        
+        for i in range(1,n):
+
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]) + nums[i]
+            dp[i][1] = dp[i-1][0]  - nums[i]
+
+        return max(dp[n-1][0],dp[n-1][1])
+            
+           
+               
