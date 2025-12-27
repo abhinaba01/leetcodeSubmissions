7a28e414bf0312eb870e228ee7fd626bdcc5c844
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2025-12-27 23:24:27 +0530

    Time: 363 ms (22.64%) | Memory: 51.3 MB (37.11%) - LeetSync

---
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.py
@@ -0,0 +1,42 @@
+class TreeAncestor:
+
+    def __init__(self, n: int, parent: List[int]):
+
+        self.dp = [[-1] * 20 for _ in range(n)]
+
+      
+
+        for i in range(len(parent)):
+            self.dp[i][0] = parent[i]
+        
+       
+        for j in range(1,20):
+            for i in range(n):
+                if self.dp[i][j-1] != -1:
+                    self.dp[i][j] = self.dp[self.dp[i][j-1]][j-1]
+                else:
+                    self.dp[i][j] = -1
+
+
+            
+    def getKthAncestor(self, node: int, k: int) -> int:
+
+        j = 0
+        while k != 0 and node != -1:
+            if k & 1:
+                node = self.dp[node][j]
+            k = k >> 1
+            j += 1
+        
+        return node
+
+            
+        
+       
+
+        
+
+
+# Your TreeAncestor object will be instantiated and called as such:
+# obj = TreeAncestor(n, parent)
+# param_1 = obj.getKthAncestor(node,k)
