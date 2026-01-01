cec3ac68e4a284165eead64d91e29ae75e785fb6
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-01-01 08:59:14 +0530

    Time: 19 ms (42.79%) | Memory: 19.7 MB (91.54%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,17 +3,14 @@ class Solution:
 
         n = len(ratings)
 
-        candy = [0] * n
-        candy[0] = 1
-
+        candy = [1] * n
+       
         for i in range(1,n):
             if ratings[i] > ratings[i-1]:
                 candy[i] = candy[i-1] + 1
-            if ratings[i] < ratings[i-1]:
-                candy[i] = 1
-            if ratings[i] == ratings[i-1]:
-                candy[i] = 1
+            
 
+           
         for i in range(n-2,-1,-1):
             if ratings[i] > ratings[i+1]:
                 candy[i] = max(candy[i],candy[i+1] + 1)
