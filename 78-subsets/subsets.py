c78674c88bcb1116e31def23fbaaa878b180b1cd
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-02-06 15:47:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (16.74%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,45 +1,24 @@
-
-
-
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
-        # n = len(nums)
-        # ans = []
-        # path = []
+        ans = []
       
 
-        # def dfs(start):
-        #     ans.append(path[:])
-        #     if start == n:
-                
-              
-        #         return
-        #     for j in range(start,n):
-        #         path.append(nums[j])
-        #         dfs(j+1)
-        #         path.pop()
-        # dfs(0)
-        # return ans
-
         n = len(nums)
-        ans = []
 
-        def subSetHelper(index:int,path:List[int]):
-            if index == len(nums):
+        def dfs(j,path):
+
+            if j == n:
                 ans.append(path)
                 return
             
-            
-            path.append(nums[index])
-            subSetHelper(index + 1,path[:])
-            path.pop()
-            subSetHelper(index + 1,path[:])
-
-        subSetHelper(0,[])
-        return ans
-            
-
 
         
+            path.append(nums[j])
+            dfs(j+1,path[:])
+            path.pop()
+            dfs(j+1,path[:])
 
+        dfs(0,[])
+
+        return ans
