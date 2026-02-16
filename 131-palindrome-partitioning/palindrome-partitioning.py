82a1145e1baeba3b3c7ae1b3f52060bfcb565ffd
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-02-16 19:04:41 +0530

    Time: 51 ms (35.65%) | Memory: 33.5 MB (77.12%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,35 +1,36 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
 
-        l = len(s)
-        ans = []
+        n = len(s)
+        res = []
+        path = []
 
-        def isPalindrome(s1:str):
-            return s1 == s1[::-1]
+        def solve(i):
 
-        def dfs(i,path):
-            if i == l:
+            if i == n:
+                res.append(path[:])
 
-                for part in path:
-                    if not isPalindrome(part):
-                        return False
-                
-                ans.append(path[:])
-                return 
             
-            for j in range(i,l):
-                part = s[i:j+1]
-                path.append(part)
-                dfs(j+1,path)
-                path.pop()
-        
-        dfs(0,[])
-        return ans
 
-        
+            for j in range(i,n):
+                temp = s[i:j+1]
+                if temp == temp[::-1]:
+                    path.append(s[i:j+1])
+                    solve(j+1)
+                    path.pop()
 
-        
-        
+        solve(0)
+        return  res
+            
+
+
+            
+
+
+
+
+
+            
 
 
         
