1de47ef304a7e00aa5120403f2d823d605d0b970
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-03-12 23:39:55 +0530

    Time: 69 ms (35.00%) | Memory: 23 MB (98.64%) - LeetSync

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+
+        n = len(nums)
+        c = 0
+
+        diff = [0] * (n + 1)
+        flip = 0
+
+        for i in range(n):
+
+            flip += diff[i]
+
+            if (nums[i] == 0 and flip % 2 == 0) or (nums[i] == 1 and flip % 2 != 0):
+
+                if i + k > n:
+                    return -1
+
+                diff[i] += 1
+                diff[i + k] -= 1
+
+                flip += 1
+                c += 1
+
+        return c
