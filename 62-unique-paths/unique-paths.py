4e0d0eada955370e1b9caa2e29047cbb4f93d931
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-04-03 12:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.64%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,20 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        @cache
-        def count(i,j):
+        dp = [[-1] * n for _ in range(m)]
 
-            if i >= m:
-                return 0
-            if j >= n:
+        def count(r,c):
+
+           
+
+            if r == m or c == n:
                 return 0
 
-            if i == m -1 and j == n -1:
+            if r == m - 1 and c == n - 1:
                 return 1
-            
-            else:
 
-                return count(i+1,j) + count(i,j+1)
+            if dp[r][c] != -1:
+                return dp[r][c]
+
+
+            dp[r][c] = count(r+1,c) + count(r,c+1)
+            return dp[r][c]
+
         
         return count(0,0)
-        
+
+
+       
