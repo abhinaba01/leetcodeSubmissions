20df1728c5a8df5fd82eaa31c0cb9c3cf38590c2
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-05-04 16:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (81.89%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,48 +1,37 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
 
-
         low , high = max(nums) , sum(nums)
-        
-        if len(nums) <k:
-            return -1
-        
-        def maxPages(limit):
-            cnt = 0
-            pages = 0
+
+        def count(limit):
+
+            cnt = 1 
+            total = 0
+
             for el in nums:
-                if pages + el > limit:
+                if total + el > limit:
                     cnt += 1
-                    pages = 0
-                
-                pages += el
-                    
-          
-            
-            
-            return cnt + 1
-                    
-                    
-                
-                
-        
+                    total = 0
+                  
+                total += el
+
+            return cnt
+
         while low <= high:
-            mid = low + (high - low)// 2
-            
-            if maxPages(mid) > k:
+            mid = low + (high - low) // 2
+            if count(mid) > k:
                 low = mid + 1
-            else:
+
+            else: 
                 high = mid - 1
-                
+           
+
         return low
+
+
+
             
-                
-            
+
         
 
-
-
-
-
-     
         
