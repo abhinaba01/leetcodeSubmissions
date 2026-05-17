f9c9a4f6d13a5bbd49f05a99d1f4a4d2cff8029e
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-05-17 19:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (41.46%) - LeetSync

--- 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.py
+++ 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.py
@@ -1,16 +1,26 @@
 class Solution:
     def minFlips(self, s: str) -> int:
+
         n = len(s)
-        cnt0 = s.count('0')
-        cnt1 = n - cnt0
+        if n < 3:
+            return 0
 
-        ans = cnt0
+        cnt = 0
 
-       
-        ans = min(ans, max(cnt1 - 1, 0))
+        ones = s.count("1")
+        zeros = n - ones
 
+        if zeros == 0 or ones <= 1:
+            return 0
         
-        keep_boundary_ones = (1 if s[0] == '1' else 0) + (1 if s[-1] == '1' else 0)
-        ans = min(ans, max(cnt1 - keep_boundary_ones, 0))
+        
 
-        return ans
+        if ones >= 2:
+            if s[0] == '1' and s[-1] == '1':
+                return min(zeros,ones - 2,ones , ones - 1)
+
+            else:
+                return min(zeros,ones , ones - 1)
+
+         
+       
