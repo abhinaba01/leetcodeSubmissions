3404c4fc4f0d41a89d20b772fa84988ee3774461
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-05-25 22:46:11 +0530

    Time: 91 ms (35.29%) | Memory: 43.1 MB (14.71%) - LeetSync

---
+++ 1223-graph-connectivity-with-threshold/graph-connectivity-with-threshold.py
@@ -0,0 +1,63 @@
+class Solution:
+    def areConnected(self, n: int, threshold: int, queries: List[List[int]]) -> List[bool]:
+
+        class DSU:
+
+            def __init__(self,n):
+                self.parent = list(range(n + 1))
+                self.size = [1] * (n + 1)
+
+            
+            def find(self,x):
+                
+                if self.parent[x] != x:
+                    self.parent[x] = self.find(self.parent[x])
+
+                
+                return self.parent[x]
+
+            
+
+            def union(self,x,y):
+
+                px = self.find(x)
+                py = self.find(y)
+
+                if px == py:
+                    return False
+
+                if self.size[px] < self.size[py]:
+                    px,py = py,px
+                
+
+                self.parent[py] = px
+                self.size[px] += self.size[py]
+
+                return True
+
+        dsu = DSU(n)
+
+        for i in range(max(threshold,1),n + 1):
+
+            for j in range(i,n + 1,i):
+
+               
+
+                if i == j:
+                    continue
+
+                if gcd(i,j) > threshold:
+                    dsu.union(i,j)
+
+
+        res = []
+
+        for u , v in queries:
+
+            if dsu.find(u) == dsu.find(v):
+                res.append(True)
+            else:
+                res.append(False)
+
+        return res
+
