96ac7a1d58b1e3fa4262570ff48aa85549946f7d
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-05-31 07:35:45 +0530

    Time: 323 ms (95.70%) | Memory: 21.4 MB (27.21%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,21 +1,36 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
 
-        delay = [float("inf")] * (n+1)
-        delay[k] = 0
+        adj = defaultdict(list)
 
-        for i in range(n-1):
-            tmpTime = delay.copy()
-            for src,dest,t in  times:
-                if delay[src] == float("inf"):
-                    continue
-                if delay[src] + t < tmpTime[dest]:
-                    tmpTime[dest] = delay[src] + t
-            delay = tmpTime
-
-       
-        ans = max(delay[1:])
+        for u , v , w in times:
+            adj[u].append((v,w))
         
+        pq = [(0,k)]
+
+        dist = [float("inf")] * (n + 1)
+        dist[k] = 0
+
+        while pq:
+
+            curr_dist , curr_node = heapq.heappop(pq)
+            
+
+            if curr_dist > dist[curr_node]:
+                continue
+            
+
+            for nei , wt in adj[curr_node]:
+                distance = curr_dist + wt
+
+                if distance < dist[nei]:
+                    dist[nei] = distance
+                    heapq.heappush(pq,(distance,nei))
+
+        
+        ans = max(dist[1:])
         return -1 if ans == float("inf") else ans
-                
+
+                    
+
         
