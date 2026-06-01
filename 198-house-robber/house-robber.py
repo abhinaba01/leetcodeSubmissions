cce794aa80ef9de1d0a3dc2bbfce2e5cdb99d5d9
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-06-01 10:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.14%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,20 +1,35 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
 
+        
         n = len(nums)
 
-        if n <= 2:
-            return max(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0],nums[1])
+        INF = float("-inf")
 
-
-        dp = [0] * (n+1)
+        dp = [INF] * n
         dp[0] = nums[0]
-        dp[1] = max(dp[0],nums[1])
+        dp[1] = max(nums[0],nums[1])
 
-        for i in range(2,n):
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i])
         
-        return dp[n-1]
+     
 
+        def f(i):
 
-       
+            if dp[i] != INF:
+                return dp[i]
+
+            ans = max(f(i-1), f(i-2) + nums[i])
+            
+            dp[i] = ans
+
+            return ans
+
+        
+
+        return f(n - 1)
+
+        
