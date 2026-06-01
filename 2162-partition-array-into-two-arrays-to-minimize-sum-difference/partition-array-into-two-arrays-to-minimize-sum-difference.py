ca6696f5137e0e4b86e46b3ac94c538a14f6b048
Abhinaba Mandal <46444159+abhinaba01@users.noreply.github.com>
2026-06-01 14:23:55 +0530

    Time: 7706 ms (5.11%) | Memory: 191.2 MB (5.43%) - LeetSync

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.py
@@ -0,0 +1,152 @@
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        total = sum(nums)
+
+        target = total / 2
+        mid = n // 2
+
+        dp1 , dp2 = {} , {}
+
+        def calc(arr):
+
+            res = defaultdict(list)
+            n = len(arr)
+
+
+
+            def solve(i,total,k):
+
+                if  i == len(arr):
+                    res[k].append(total)
+                    return
+                    
+                solve(i + 1, total + arr[i],k + 1)
+
+                solve(i + 1, total , k)
+
+            solve(0,0,0)
+
+            return res
+
+
+        dp1 = calc(nums[:mid])
+        dp2 = calc(nums[mid:])
+
+        dp1[0] = [0]
+        dp2[0] = [0]
+
+        
+
+
+
+        
+
+        
+        
+        ansk = [0] * (mid + 1)
+        for k in range(mid + 1):
+
+            arr1 = dp1[k]
+            arr2 = dp2[mid - k]
+
+            arr1.sort()
+            arr2.sort()
+
+          
+            res = float("inf")
+
+            ansi = [0] * len(arr1)
+            for i in range(len(arr1)):
+
+                x = target - arr1[i]
+
+                low , high = 0 , len(arr2) - 1
+
+                prev , nxt = None , None
+                ans= -1
+
+                while low <= high:
+                    m = low + (high - low) // 2
+                  
+                    if arr2[m] <= x :
+                        ans = m
+                        low = m + 1
+                    elif arr2[m] > x :
+                        high = m - 1
+
+                if ans != -1:
+                    prev = arr2[ans]
+
+                low , high = 0 , len(arr2) - 1 
+                ans = -1
+                while low <= high:
+                    m = low + (high - low) // 2
+                 
+                    if arr2[m] < x :
+                        low = m + 1
+                    elif arr2[m] >= x :
+                        ans = m
+                        high = m - 1
+                        
+                if ans != -1:
+                    nxt = arr2[ans]
+
+                best = float("inf")
+
+                if prev is not None :
+                    best = min(
+                        best,
+                        abs(total - 2 * (arr1[i] + prev))
+                    )
+
+                if nxt is not None :
+                    best = min(
+                        best,
+                        abs(total - 2 * (arr1[i] + nxt))
+                    )
+
+                ansi[i] = best
+
+            
+            ansk[k] = min(ansi)
+
+        return min(ansk)
+
+            
+
+            
+
+
+
+
+
+
+                    
+
+                
+
+            
+
+
+
+                
+
+
+
+
+
+
+            
+
+        
+
+
+
+                
+                    
+
+
+            
+        
